ae766589dc8f483c2c742170d8be75b494459615
Kisan Shah <kisan@blackcurrantlabs.com>
2023-11-23 18:38:14 +0530

    feat: add drawe in home page

--- lib/features/home/ui/components/home_app_bar.dart
+++ lib/features/home/ui/components/home_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce_front_end/features/routes/router/app_router.gr.dart';
 import 'package:e_commerce_front_end/features/shared/providers/router_provider.dart';
 import 'package:e_commerce_front_end/features/theme/app_color.dart';
+import 'package:e_commerce_front_end/features/utils/app_const.dart';
 import 'package:e_commerce_front_end/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -18,7 +19,12 @@ class HomeAppBar extends ConsumerWidget implements PreferredSizeWidget {
     return AppBar(
       elevation: 0,
       backgroundColor: Colors.transparent,
-      leading: Assets.svg.menu.svg(fit: BoxFit.scaleDown),
+      leading: GestureDetector(
+        child: Assets.svg.menu.svg(fit: BoxFit.scaleDown),
+        onTap: () {
+          scaffoldKey.currentState?.openDrawer();
+        },
+      ),
       centerTitle: true,
       title: Text(
         title,
--- lib/features/main/main_page.dart
+++ lib/features/main/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:e_commerce_front_end/features/main/components/bottom_nav_bar.dart';
 import 'package:e_commerce_front_end/features/routes/router/app_router.gr.dart';
+import 'package:e_commerce_front_end/features/utils/app_const.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -25,6 +26,11 @@ class _MainPageState extends ConsumerState<MainPage> {
     return SafeArea(
       top: false,
       child: AutoTabsScaffold(
+        scaffoldKey: scaffoldKey,
+        drawer: Drawer(
+          elevation: 0,
+          child: Container(),
+        ),
         routes: const [
           HomeRoute(),
           // SearchRoute(),
